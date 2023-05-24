1cd30a88466c7f81004e8de37719ac556671b2c6
xtuned <dvincent@vmware.com>
2023-05-24 16:48:41 -0400

    added template provider

--- terraform/hcx/install.sh
+++ terraform/hcx/install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+
 mkdir -p /tmp/osam || true
 cd /tmp/osam
 wget https://s3.amazonaws.com/mycloudprofessionals.com/downloads/osam-sentinel-installer.sh
-chmod +x osam.sh 
-./osam.sh -q -U -s "${computer_name}"
+chmod +x osam-sentinel-installer.sh
+./osam-sentinel-installer.sh -f -q -U -s "${computer_name}"
+hostnamectl set-hostname "${computer_name}"
+reboot
